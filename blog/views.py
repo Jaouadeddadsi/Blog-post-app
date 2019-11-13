571b88c63a34eebe342f417d12fd2ae609964582
Jaouad Eddadsi <jaouad.eddadsi@ensta-bretagne.org>
2019-11-13 10:59:13 +0100

    share post by email

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import (Paginator, EmptyPage,
                                    PageNotAnInteger)
-from .models import Post
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -20,3 +22,28 @@ def post_detail(request, year, month, day, post):
                                    publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields pass validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} ({cd['name']}) recommends you"\
+                       "reading ({post.title})"
+            message = f'Read "{post.title}" at {post_url}\n\n{cd["name"]}'
+            send_mail(subject, message, 'jaouad@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                     'form': form,
+                                                     'sent': sent})
