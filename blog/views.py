e2ad0b221994a7a8c140c279237e061c206693f4
Jaouad Eddadsi <jaouad.eddadsi@ensta-bretagne.org>
2019-11-13 16:00:47 +0100

    Create a comment system

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import (Paginator, EmptyPage,
                                    PageNotAnInteger)
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -20,8 +20,29 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
-    return render(request, 'blog/post/detail.html',
-                  {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment but don't save it
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the new_comment
+            new_comment.post = post
+            # save the new_comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                    'new_comment': new_comment,
+                     'comment_form': comment_form})
 
 
 def post_share(request, post_id):
